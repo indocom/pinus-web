238078c88bd40479dd3ec24884b5b51e307ec9cc
benclmnt <bennettclement@gmail.com>
2019-10-05 09:15:53 +0800

    initial template

---
+++ js/init.js
@@ -0,0 +1,16 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.material-boxed').materialbox();
+    $('.parallax').parallax();
+    $('.tabs').tabs();
+    $('.datepicker').datepicker({
+      disableWeekends: true
+    });
+    $('.tooltip').tooltip();
+    $('.scrollspy').scrollSpy();
+    $('.carousel').carousel();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
