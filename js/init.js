625a4c43702ee449decb096ef238ced8f7b989f1
benclmnt <bennettclement@gmail.com>
2019-10-12 10:47:48 +0800

    add full screen landing page

--- js/init.js
+++ js/init.js
@@ -14,3 +14,10 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+const headerimage = document.querySelector('#landing::after');
+window.addEventListener('scroll', () => {
+  headerimage.style.opacity = 1 - (window.pageYOffset - 300) / 550;
+  console.log(headerimage);
+
+})
