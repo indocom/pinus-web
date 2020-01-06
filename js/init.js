f90ac316628b3a62df46bb8be6df8fcb98b8c9fa
Audrey Felicio Anwar <54733401+AudreyFelicio@users.noreply.github.com>
2020-01-06 19:52:50 +0700

    Update effects (#3)
    * change hover effects on team cards
    
    * add full screen landing page
    
    * add 'return to top' button
    
    * add filter to landing page and team members cards
    
    * Add arrow at landing page and resolve some PR changes
    
    * Move about us and vision mission layout
    
    * Improve footer, initiative card, cleanup comments
    
    * Change icons
    
    * Add sticky nav
    
    * Add tooltip animation
    
    * Add in some info & change some footer styling
    
    * Added collapsible read more
    
    * Added Read More that opens a pop up containing the full description of the events and hover effects on the Read More texts
    
    Co-authored-by: benClmnt <49342399+benClmnt@users.noreply.github.com>
    Co-authored-by: Steven Wijaya <stevenwjy149@gmail.com>

--- js/init.js
+++ js/init.js
@@ -11,6 +11,11 @@
     $('.tooltip').tooltip();
     $('.scrollspy').scrollSpy();
     $('.carousel').carousel();
-
+    $(document).ready(function(){
+      $('.collapsible').collapsible();
+    });
+    $(document).ready(function(){
+      $('.modal').modal();
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
