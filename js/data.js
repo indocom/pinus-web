1e2a42535052e61559f2b619833701fb49cc5310
Steven Wijaya <stevenwjy149@gmail.com>
2020-01-19 18:14:02 +0800

    Add updated content data

---
+++ js/data.js
@@ -0,0 +1,246 @@
+var externalEventsData = [
+  {
+    title: 'Pekan Olahraga Pelajar Indonesia (POPI)',
+    link: 'https://www.facebook.com/pinusPOPI/',
+    readMore: `
+      Pekan Olahraga Pelajar Indonesia (POPI) is an annual sports charity
+      event between Indonesian students’ association from different
+      universities and Junior Colleges around Singapore. The proceeds from
+      the event partly goes to the Misi Kami Peduli (Mission: We Care) team,
+      which organises an Overseas Community Involvement Programme in
+      Indonesia.
+    `,
+    shortDesc: `
+      Pekan Olahraga Pelajar Indonesia (POPI) is an annual sports ...
+    `
+  },
+  {
+    title: 'NUANSA Cultural Productions',
+    link: 'https://www.nuansacp.com/',
+    readMore: `
+      NUANSA is a cultural production group based in the National University
+      of Singapore. Through contemporary musical theatre, we attempt to
+      showcase the beauty and diversity of Indonesian cultures. We
+      consistently endeavor to bring the audience through an immersive
+      journey by ensuring the cultural authenticity and artistic integrity
+      of our shows and events. Since our inception in 2008, we have grown
+      from strength to strength: from a small independent Indonesian
+      performance group to one of the biggest student productions in
+      Singapore, featuring a large and diverse community from different
+      nationalities.
+    `,
+    shortDesc: `
+      NUANSA is a cultural production group based in the National
+      University of Singapore. Through ...
+    `
+  },
+  {
+    title: 'Misi Kami Peduli',
+    link: 'https://www.misikamipeduli.org/',
+    readMore: `
+      Misi Kami Peduli (MKP) or Mission: We Care is a community service
+      project which aims to empower underprivileged Indonesian communities.
+      Misi Kami Peduli's projects focus on three key pillars: Education,
+      Community Empowerment and Technology to alleviate the socio-economic
+      challenges in the local communities.
+    `,
+    shortDesc: `
+      Misi Kami Peduli (MKP) or Mission: We Care is a community
+      service project which aims to empower underprivileged Indonesian ...
+    `
+  }
+]
+
+var internalEventsData = [
+  {
+    title: 'PINUS Orientation',
+    readMore: `
+      Feeling homesick? Fret not, PINUS Orientation welcomes all Indonesian 
+      freshmen. Through many fun activities planned by the committee, you will 
+      definitely make new friends. Soon, you’ll find that they are your home 
+      away from home.
+    `
+  },
+  {
+    title: 'Senior Appreciation Night',
+    readMore: `
+      Senior Appreciation Night (SAN) is an annual dinner & dance event for 
+      graduating PINUS members. Apart from being a platform to congratulate 
+      the graduating seniors, SAN is also an opportunity for bonding between 
+      Indonesian freshmen, seniors, as well as alumni in NUS.
+    `
+  },
+  {
+    title: 'Nusantaraku & BBQ',
+    readMore: `
+      PINUS Nusantaraku & BBQ aims to provide a common space for PINUS freshies 
+      to bond with their batch and with the seniors by having fun together through 
+      performance and games (Nusantaraku) and Food Party in the spirit of 
+      independence day.
+    `
+  }
+]
+
+var publicationsData = [
+  {
+    title: 'Humans of PINUS',
+    link: 'https://www.instagram.com/humansof.pinus/',
+    readMore: `
+      Eager to know someone but too busy to meet them? Interested in someone 
+      but too shy to talk to them? Humans of PINUS shares about the Indonesian 
+      Diasporas at NUS, one story at a time.
+    `
+  },
+  {
+    title: 'MANUSIA',
+    link: 'https://manusiapinus.wordpress.com/',
+    readMore: `
+      What makes a community? In MANUSIA, we explore what makes the Indonesian 
+      community unique in NUS in the form of blog posts. As manusia means human 
+      in Bahasa, MANUSIA tells the stories of Indonesian students in NUS. We are 
+      particularly interested to explore how education in Singapore transforms 
+      the idea of identity, community, and nationality among Indonesian students 
+      in NUS.
+    `
+  },
+  {
+    title: 'TanyaDiaspora',
+    link: 'https://www.youtube.com/channel/UCDu42CYYcTla2qw-JSQUWfg',
+    readMore: `
+      Tanya Diaspora is started by a group of Indonesian diasporas who are dedicated 
+      to share valuable experiences from their respective university majors with our 
+      viewers (high school students, etc).  We hope that our videos can shed light on 
+      multitude opportunities that arise from joining these majors.
+    `
+  }
+]
+
+var topFourData = [
+  {
+    name: 'Nadhira Salsabila',
+    role: ''
+  },
+  {
+    name: 'Amelinda Gusrianti Harahap',
+    role: ''
+  },
+  {
+    name: 'Michael Gunawan',
+    role: ''
+  },
+  {
+    name: 'Kezia Putri',
+    role: ''
+  }
+]
+
+var divisionsData = [
+  {
+    name: 'Public and Alumni Relations',
+    members: [
+      {
+        name: 'Aldo',
+        role: 'Director'
+      },
+      {
+        name: 'Jeff Ivanos Gana',
+        role: 'Director'
+      },
+      {
+        name: 'Aldrich Williams',
+        role: 'Member'
+      },
+      {
+        name: 'Archangel Quinterra',
+        role: 'Member'
+      },
+      {
+        name: 'Jovita Suryawinata',
+        role: 'Member'
+      },
+      {
+        name: 'Kathy Fresilia Ijaya',
+        role: 'Member'
+      },
+      {
+        name: 'Kenisha Alicia',
+        role: 'Member'
+      }
+    ]
+  },
+  {
+    name: 'Technology',
+    members: [
+      {
+        name: 'Steven Wijaya',
+        role: 'Director'
+      },
+      {
+        name: 'Audrey Felicio Anwar',
+        role: 'Member'
+      },
+      {
+        name: 'Bennett Clement',
+        role: 'Member'
+      },
+      {
+        name: 'Kevin William',
+        role: 'Member'
+      }
+    ]
+  },
+  {
+    name: 'Publicity',
+    members: [
+      {
+        name: 'Dipasukha Edbert',
+        role: 'Member'
+      },
+      {
+        name: 'Erin May Gunawan',
+        role: 'Member'
+      },
+      {
+        name: 'Felix Halim',
+        role: 'Member'
+      }
+    ]
+  },
+  {
+    name: 'Welfare',
+    members: [
+      {
+        name: 'Hokiewan',
+        role: 'Director'
+      },
+      {
+        name: 'Nathanael Septianto',
+        role: 'Member'
+      },
+      {
+        name: 'Agatha Felice',
+        role: 'Member'
+      },
+      {
+        name: 'Albert Reinardo',
+        role: 'Member'
+      },
+      {
+        name: 'Alicia Rachel',
+        role: 'Member'
+      },
+      {
+        name: 'Alvin Gunawan',
+        role: 'Member'
+      },
+      {
+        name: 'Christian Drake Martin',
+        role: 'Member'
+      },
+      {
+        name: 'Cindy Aurelia Ursia',
+        role: 'Member'
+      }
+    ]
+  }
+]
