9047689e3c95e53b8e48803548be4fdfb8dad14c
benClmnt <49342399+benClmnt@users.noreply.github.com>
2020-03-01 03:26:13 +0800

    Fix Navbar (#14)
    * Fix sidenav
    
    * Fix nav text out of container in ipad pro + fix navbar in mobile mode
    
    * Navbar stays on top after scroll
    
    * Put navbar on top for small devices
    
    * Update images
    
    Co-authored-by: Steven Wijaya <stevenwjy149@gmail.com>

--- js/init.js
+++ js/init.js
@@ -22,3 +22,21 @@
 document.querySelector("#landing_arrow").addEventListener("click", function() {
   document.querySelector("#landing_empty_div").click();
 });
+
+var nav = document.querySelector('.nav-container');
+var stickyPosition = nav.offsetTop;
+
+var stickyNav = document.getElementById('sticky-navbar');
+stickyNav.innerHTML = nav.innerHTML;
+stickyNav.style = "display: none; padding: .7em; background: #222;"
+stickyNav.classList.add('sticky-nav');
+
+window.addEventListener('scroll', function() {
+  if (window.pageYOffset > stickyPosition) {
+    nav.style.display = 'none';
+    stickyNav.style.display = 'block';
+  } else {
+    nav.style.display = 'block';
+    stickyNav.style.display = 'none';
+  }
+});
